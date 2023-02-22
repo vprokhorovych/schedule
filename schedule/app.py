85b0f96642fd2be3e758487546a797b7809b371a
Viacheslav Prokhorovych <p.slava.ih@gmail.com>
2023-02-22 12:11:00 +0200

    initial commit

---
+++ schedule/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/main")
+def main():
+    return render_template('main.html')
+    # return "<p>Hello, World!</p>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
